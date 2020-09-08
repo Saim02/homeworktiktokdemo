112138a4751b9b437f90d5eaddd459a7e2ebaca6
Saim Ahmed <ahmed@Comp.local>
2020-09-08 16:31:55 -0400

    added demodata

---
+++ assets/demoData/videosArray.js
@@ -0,0 +1,135 @@
+import video1 from '../videos/video3.mp4';
+import video2 from '../videos/video11.mp4';
+import video3 from '../videos/video6.mp4';
+import video4 from '../videos/video10.mp4';
+import video5 from '../videos/video12.mp4';
+import video6 from '../videos/video5.mp4';
+import video7 from '../videos/video9.mp4';
+import video8 from '../videos/video123.mp4';
+import video9 from '../videos/video13.mp4';
+
+const videosArray = [
+  {
+    id: 'video1',
+    video: video2,
+    liked: false,
+    totalLikes: 200,
+    paused: true,
+    comments: [
+      {id: 'comment 1', comment: 'Comment in the name'},
+      {id: 'comment 2', comment: 'Comment in the last'},
+      {id: 'comment 3', comment: 'Comment in the name'},
+      {id: 'comment 4', comment: 'Comment in the last'},
+      {id: 'comment 5', comment: 'Comment in the last'},
+      {id: 'comment 6', comment: 'Comment in the name'},
+      {id: 'comment 7', comment: 'Comment in the last'},
+      {id: 'comment 8', comment: 'Comment in the name'},
+      {id: 'comment 9', comment: 'Comment in the last'},
+      {id: 'comment 10', comment: 'Comment in the name'},
+      {id: 'comment 11', comment: 'Comment in the name'},
+      {id: 'comment12', comment: 'Comment in the last'},
+      {id: 'comment13', comment: 'Comment in the last'},
+      {id: 'comment14', comment: 'Comment in the name'},
+      {id: 'comment15', comment: 'Comment in the last'},
+      {id: 'comment16', comment: 'Comment in the name'},
+      {id: 'comment17', comment: 'Comment in the last'},
+      {id: 'comment18', comment: 'Comment in the name'},
+    ],
+  },
+  {
+    id: 'video2',
+    video: video1,
+    liked: false,
+    paused: true,
+    totalLikes: 130,
+
+    comments: [
+      {id: 'comment 1', comment: 'Comment in the name'},
+      {id: 'comment 2', comment: 'Comment in the last'},
+    ],
+  },
+  {
+    id: 'video3',
+    video: video3,
+    liked: false,
+    paused: true,
+    totalLikes: 120,
+
+    comments: [
+      {id: 'comment 1', comment: 'Comment in the name'},
+      {id: 'comment 2', comment: 'Comment in the last'},
+    ],
+  },
+  {
+    id: 'video4',
+    video: video4,
+    liked: false,
+    paused: true,
+    totalLikes: 200,
+
+    comments: [
+      {id: 'comment 1', comment: 'Comment in the name'},
+      {id: 'comment 2', comment: 'Comment in the last'},
+    ],
+  },
+  {
+    id: 'video5',
+    video: video5,
+    liked: false,
+    paused: true,
+    totalLikes: 500,
+
+    comments: [
+      {id: 'comment 1', comment: 'Comment in the name'},
+      {id: 'comment 2', comment: 'Comment in the last'},
+    ],
+  },
+  {
+    id: 'video6',
+    video: video6,
+    liked: false,
+    paused: true,
+    totalLikes: 760,
+
+    comments: [
+      {id: 'comment 1', comment: 'Comment in the name'},
+      {id: 'comment 2', comment: 'Comment in the last'},
+    ],
+  },
+  {
+    id: 'video7',
+    video: video7,
+    liked: false,
+    paused: true,
+    totalLikes: 800,
+
+    comments: [
+      {id: 'comment 1', comment: 'Comment in the name'},
+      {id: 'comment 2', comment: 'Comment in the last'},
+    ],
+  },
+  {
+    id: 'video8',
+    video: video8,
+    liked: false,
+    paused: true,
+    totalLikes: 122,
+    comments: [
+      {id: 'comment 1', comment: 'Comment in the name'},
+      {id: 'comment 2', comment: 'Comment in the last'},
+    ],
+  },
+  {
+    id: 'video9',
+    video: video9,
+    liked: false,
+    paused: true,
+    totalLikes: 600,
+    comments: [
+      {id: 'comment 1', comment: 'Comment in the name'},
+      {id: 'comment 2', comment: 'Comment in the last'},
+    ],
+  },
+];
+
+export default videosArray;
